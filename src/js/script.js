7eb4a7ac8e88b56597c0ddfcd7997f5ca8030462
unknown <kasimsarman@gmail.com>
2026-05-06 20:08:52 +0500

    CREATE section sidepanel and menu + js menu code

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 import "../sass/style.scss";
+
+const burger = document.querySelector(".burger"),
+  menu = document.querySelector(".menu"),
+  close = document.querySelector(".menu__close");
+
+burger.addEventListener("click", () => {
+  menu.classList.add("active");
+});
+
+close.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
